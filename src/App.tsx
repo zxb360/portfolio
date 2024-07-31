38a9f6410382e82046cb6804cd47fdb84688a532
jaeder azevedo <jaeder@debian.ziza>
2024-07-30 23:36:23 -0300

    Subindo projeto pessoal

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MainHome from './page/mainhome/MainHome'
+
+function App() {
+  return (
+    <>
+      <MainHome />
+    </>
+  )
+}
+
+export default App
---
+++ src/page/mainhome/MainHome.tsx
@@ -0,0 +1,52 @@
+import './HomeMain.css'
+
+function MainHome() {
+  return (
+    <>
+        <img src="../../public/fotoJaeder.png" alt="foto de Jaeder" />
+        <header>
+          <strong>Jaeder de Carvalho Azevedo</strong>
+          <p>Formação: Analista e Desenvolvedor Sistema</p>
+          <p>Curso de especialização: Trybe</p>
+          <p>Develop: FrontEnd, BackEnd and mid data science</p>
+          <p>Buscando: Primeiro emprego na área</p>
+        </header>
+        <section>
+          <div>
+            <h3>2017</h3>
+            <h2>Conhecendo a programação</h2>
+            <p>Sempre tive muita vontade de conhecer os estudos em programção
+              e o curso em video foi quem iniciou minha a minha jornada de curiosidade,
+              assistindo seus videos com tempo que tinha consegue identificar a área de gostaria de caminhar
+            </p>
+          </div>
+          <div>
+            <h3>2018 a 2021</h3>
+            <h2>Faculdade em Analise e Desenvolvimento de Sistemas</h2>
+            <h4>Estácio Fib</h4>
+            <p>
+              Depois que finalizei minha formação no ensino médio fique sem estudar praticamente 18 anos,
+              somente em 2018 consegue voltar a estudar para uma jornada de apredizado no mundo da programação.
+              Conheci teorias, fundamentos de estrutura de dados, lógica programação I e II, web sites, gestão de Empreendedorismo
+              e trabalho de Conclusão projeto final.
+            </p>
+          </div>
+          <div>
+            <h3>2021</h3>
+            <p>Curso de pós graduação</p>
+          </div>
+          <div>
+            <h3>2022</h3>
+            <p>Curso na trybe</p>
+          </div>
+        </section>
+        <h2>Projetos</h2>
+        <h1>secao de projetos e exercicios</h1>
+        <footer>
+          <p>links das redes sociais imagens redondas com logo redirecionando ao meu perfil</p>
+        </footer>
+    </>
+  )
+}
+
+export default MainHome
